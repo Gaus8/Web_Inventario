55d1ffd1febdc54feb1bc4c79b3ecf548152ea97
FGordillo12 <egordillo@ucundinamarca.edu.co>
2025-05-12 21:11:18 -0500

    Palabras en negrilla en mensaje de verificacion de cuenta

--- backend/funciones/validarEmail.js
+++ backend/funciones/validarEmail.js
@@ -17,20 +17,20 @@ export const enviarCorreoVerificacion = async (usuario, token) => {
   const verificationUrl = `http://localhost:5000/validacion/${token}`;
 
   const mailOptions = {
-    from: '"Tu Aplicación" <pruebascdisfruta@gmail.com>',
+    from: '"CDISFRUTA - Plataforma gestionada por AMG" <pruebascdisfruta@gmail.com>',
     to: usuario.email,
-    subject: 'Verifacion de correo electronico - Registro en CDIFRUTA',
+    subject: 'Confirmación de correo electronico - Registro en CDIFRUTA',
     html: `
-      <p>Estimado(a) ${usuario.name},</p>
-      <p>Gracias por registrarse en CDISFRUTA.</p>
-      <p><br>Para completar su proceso de registro y activar su cuenta, le solicitamos verificar su dirección de correo electrónico haciendo clic en el siguiente enlace:</br></p>
+      <p>Estimado(a) ${usuario.name}</p>
+      <p><br>Gracias por registrarse en <b>CDISFRUTA</b>.</br></p>
+      <p>Para completar su proceso de registro y activar su cuenta, le solicitamos verificar su dirección de correo electrónico haciendo clic en el siguiente enlace:</p>
       <a href="${verificationUrl}">${verificationUrl}</a>
       <p>Este paso es fundamental para confirmar que la dirección proporcionada es válida y pertenece a usted.</p>
-      <p><br>Este mensaje ha sido enviado por AMG – Aplicativo Moderno de Gestión, plataforma que respalda tecnológicamente el funcionamiento de CDISFRUTA.</br></p>
+      <p><br>Este mensaje ha sido enviado por <b>AMG – Aplicativo Moderno de Gestión</b>, plataforma que respalda tecnológicamente el funcionamiento de CDISFRUTA.</br></p>
       <p>Si usted no ha realizado este registro, puede ignorar este mensaje con toda seguridad.</p>
       <p><br>Agradecemos su confianza.</br></p>
       <p>Atentamente,</p>
-      <p>Equipo de Soporte – AMG</p>
+      <p><b>Equipo de Soporte</b> – AMG</p>
     `
   };
 

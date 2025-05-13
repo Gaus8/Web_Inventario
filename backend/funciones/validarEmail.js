99a737ef582398689d2491512a9bf6dce2518b6e
FGordillo12 <egordillo@ucundinamarca.edu.co>
2025-05-12 21:01:30 -0500

    Modificacion de texto en el mensaje de verificacion de cuenta

--- backend/funciones/validarEmail.js
+++ backend/funciones/validarEmail.js
@@ -19,12 +19,18 @@ export const enviarCorreoVerificacion = async (usuario, token) => {
   const mailOptions = {
     from: '"Tu Aplicación" <pruebascdisfruta@gmail.com>',
     to: usuario.email,
-    subject: 'Verifica tu cuenta',
+    subject: 'Verifacion de correo electronico - Registro en CDIFRUTA',
     html: `
-      <p>Hola ${usuario.name},</p>
-      <p>Gracias por registrarte. Por favor verifica tu cuenta haciendo clic en el siguiente enlace:</p>
+      <p>Estimado(a) ${usuario.name},</p>
+      <p>Gracias por registrarse en CDISFRUTA.</p>
+      <p><br>Para completar su proceso de registro y activar su cuenta, le solicitamos verificar su dirección de correo electrónico haciendo clic en el siguiente enlace:</br></p>
       <a href="${verificationUrl}">${verificationUrl}</a>
-      <p>Este enlace es válido por 24 horas.</p>
+      <p>Este paso es fundamental para confirmar que la dirección proporcionada es válida y pertenece a usted.</p>
+      <p><br>Este mensaje ha sido enviado por AMG – Aplicativo Moderno de Gestión, plataforma que respalda tecnológicamente el funcionamiento de CDISFRUTA.</br></p>
+      <p>Si usted no ha realizado este registro, puede ignorar este mensaje con toda seguridad.</p>
+      <p><br>Agradecemos su confianza.</br></p>
+      <p>Atentamente,</p>
+      <p>Equipo de Soporte – AMG</p>
     `
   };
 

cf2833fa06c84fcb020a90262c82d74505bb2ed7
Stiven Garzon <usg210802@outlook.com>
2025-11-03 19:33:08 -0500

    correccion en el login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -105,7 +105,7 @@ const validateLogin = async (req, res) => {
   res.cookie('access_token', token, {
     httpOnly: true,
     secure: true,      // false en desarrollo
-    sameSite: 'strict',    // 'lax' o 'strict' en desarrollo
+    sameSite: 'lax',    // 'lax' o 'strict' en desarrollo
     maxAge: 24 * 60 * 60 * 1000 // 24 horas
   })
     .status(200).json({

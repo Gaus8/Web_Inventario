e746c379ba3f460f4b48596407b9c6dbdd7f3d2f
Stiven Garzon <usg210802@outlook.com>
2025-11-20 13:29:56 -0500

    correccion de envio de correo

--- backend/middleware/validarEmail.js
+++ backend/middleware/validarEmail.js
@@ -1,5 +1,9 @@
 import crypto from 'crypto';
 import nodemailer from 'nodemailer';
+import 'dotenv/config'
+
+import crypto from 'crypto';
+import nodemailer from 'nodemailer';
 
 export const generarTokenVerificacion = () => {
   return crypto.randomBytes(32).toString('hex');
@@ -7,34 +11,27 @@ export const generarTokenVerificacion = () => {
 
 export const enviarCorreoVerificacion = async (usuario, token) => {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: 'gmail',
     auth: {
-      user: 'pruebascdisfruta2025@gmail.com',
-      pass: 'oxdlbhjcrqpfziuj'
+      user: process.env.EMAIL_CLIENT,
+      pass: process.env.EMAIL_PASSWORD
     }
   });
 
-  const verificationUrlReact = `https://inventario-cdisfruta.netlify.app/validacion/${token}`;
-// const verificationUrlReact = `http://localhost:5173/validacion/${token}`;
-
+  const verificationUrl = `https://inventario-cdisfruta.netlify.app/validacion/${token}`;
 
   const mailOptions = {
-    from: '"SIECU - Plataforma gestionada a CDISFRUTA" <pruebascdisfruta@gmail.com>',
+    from: `"SIECU - Plataforma gestionada a CDISFRUTA" <${process.env.MAIL_USER}>`,
     to: usuario.email,
-    subject: 'Confirmación de correo electronico - Registro en CDISFRUTA',
+    subject: 'Confirmación de correo electrónico - Registro en CDISFRUTA',
     html: `
       <p>Estimado(a) ${usuario.name}</p>
-      <p><br>Gracias por registrarse en <b>SIECU</b>.</br></p>
-      <p>Para completar su proceso de registro y activar su cuenta, le solicitamos verificar su dirección de correo electrónico haciendo clic en el siguiente enlace:</p>
-      <a href="${verificationUrlReact}">${verificationUrlReact}</a>
-      <p>Este paso es fundamental para confirmar que la dirección proporcionada es válida y pertenece a usted.</p>
-      <p><br>Este mensaje ha sido enviado por <b>SIECU – Sistema Integral de E-Commerce</b>, plataforma que respalda tecnológicamente el funcionamiento de CDISFRUTA.</br></p>
-      <p>Si usted no ha realizado este registro, puede ignorar este mensaje con toda seguridad.</p>
-      <p><br>Agradecemos su confianza.</br></p>
-      <p>Atentamente,</p>
-      <p><b>Equipo de Soporte</b> – SIECU</p>
+      <p>Gracias por registrarse en <b>SIECU</b>.</p>
+      <p>Para activar su cuenta haga clic aquí:</p>
+      <a href="${verificationUrl}">${verificationUrl}</a>
+      <p>Si no realizó este registro, ignore este mensaje.</p>
     `
   };
 
-  await transporter.sendMail(mailOptions);
+  return transporter.sendMail(mailOptions);
 };
